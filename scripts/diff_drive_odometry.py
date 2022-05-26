a42a888f43d48fb3de19ed7ce6abeab519a8e90b
Mateo Lozano Hoyos <mateolozanohoyos@gmail.com>
2022-05-26 12:42:04 +0400

    version 2.0

--- scripts/diff_drive_odometry.py
+++ scripts/diff_drive_odometry.py
@@ -1,27 +1,19 @@
 #! /usr/bin/env python
 from __future__ import division
+
 import rospy
 from geometry_msgs.msg import Quaternion
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Int16
-from sensor_msgs.msg import Imu
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from tf.broadcaster import TransformBroadcaster
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
 from math import sin, cos
-#from diff_drive.pose import Pose
+from diff_drive.pose import Pose
 from diff_drive import odometry
 right = 0.0
 left = 0.0
-sequence=0
-frameid=""
-angx=0.0
-angy=0.0
-angz=0.0
-linx=0.0
-liny=0.0
-linz=0.0
 
 
 class OdometryNode:
@@ -31,7 +23,6 @@ class OdometryNode:
 
     def main(self):
         self.odomPub = rospy.Publisher('odom', Odometry, queue_size=10)
-        # self.imuPub = rospy.Publisher('imu_data', Imu, queue_size=10)
         #self.tfPub = TransformBroadcaster()
 
         rospy.init_node('diff_drive_odometry')
@@ -40,18 +31,16 @@ class OdometryNode:
 
         rospy.Subscriber("lwheel", Int16, self.leftCallback)
         rospy.Subscriber("rwheel", Int16, self.rightCallback)
-        # rospy.Subscriber("imu", Imu, self.imu)
-        #rospy.Subscriber("initialpose", PoseWithCovarianceStamped,
-        #                self.on_initial_pose)
+        # rospy.Subscriber("initialpose", PoseWithCovarianceStamped,
+        #                  self.on_initial_pose)
 
         self.ticksPerMeter = int(rospy.get_param('~ticks_meter'))
         self.wheelSeparation = float(rospy.get_param('~base_width'))
-        self.rate = float(rospy.get_param('~rate', 33.0))
+        self.rate = float(rospy.get_param('~rate', 10.0))
         self.baseFrameID = rospy.get_param('~base_frame_id', 'base_link')
         self.odomFrameID = rospy.get_param('~odom_frame_id', 'odom')
         self.encoderMin = int(rospy.get_param('~encoder_min', -32768))
         self.encoderMax = int(rospy.get_param('~encoder_max', 32767))
-        self.tfPub=False
 
         self.odometry.setWheelSeparation(self.wheelSeparation)
         self.odometry.setTicksPerMeter(self.ticksPerMeter)
@@ -64,8 +53,6 @@ class OdometryNode:
             rate.sleep()
 
     def publish(self):
-        # global sequence, frameid, angx, angy, angz, linx, liny, linz
-        
         self.odometry.updatePose(rospy.get_time())
         now = rospy.get_rostime()
         pose = self.odometry.getPose()
@@ -91,71 +78,35 @@ class OdometryNode:
         odom.pose.pose.orientation.w = q[3]
         odom.twist.twist.linear.x = pose.xVel
         odom.twist.twist.angular.z = pose.thetaVel
-        odom.pose.covariance = [.01, 0, 0, 0, 0, 0,
-                                 0, .01, 0, 0, 0, 0,
-                                 0, 0, .01, 0, 0, 0,
-                                 0, 0, 0, .01, 0, 0,
-                                 0, 0, 0, 0, .01, 0,
-                                 0, 0, 0, 0, 0, .01]
-        # imu=Imu()
-        # imu.header.seq=sequence
-        # imu.header.stamp=now
-        # imu.header.frame_id=frameid
-        # imu.angular_velocity.x=angx
-        # imu.angular_velocity.y=angy
-        # imu.angular_velocity.z=angz
-        # imu.linear_acceleration.x=linx
-        # imu.linear_acceleration.y=liny
-        # imu.linear_acceleration.z=linz
-        # imu.linear_acceleration_covariance=[.01, 0, 0,
-        #                                       0, .01, 0,
-        #                                       0, 0, .01]
-        # imu.angular_velocity_covariance=[.01, 0, 0,
-        #                                    0, .01, 0,
-        #                                    0, 0, .01]
-        
-        
         self.odomPub.publish(odom)
-        # self.imuPub.publish(imu)
 
-    def on_initial_pose(self, msg):
-        q = [msg.pose.pose.orientation.x,
-             msg.pose.pose.orientation.x,
-             msg.pose.pose.orientation.x,
-             msg.pose.pose.orientation.w]
-        roll, pitch, yaw = euler_from_quaternion(q)
+    # def on_initial_pose(self, msg):
+    #     q = [msg.pose.pose.orientation.x,
+    #          msg.pose.pose.orientation.x,
+    #          msg.pose.pose.orientation.x,
+    #          msg.pose.pose.orientation.w]
+    #     roll, pitch, yaw = euler_from_quaternion(q)
 
-        pose = Pose()
-        pose.x = msg.pose.pose.position.x
-        pose.y = msg.pose.pose.position.y
-        pose.theta = yaw
+    #     pose = Pose()
+    #     pose.x = msg.pose.pose.position.x
+    #     pose.y = msg.pose.pose.position.y
+    #     pose.theta = yaw
 
-        rospy.loginfo('Setting initial pose to %s', pose)
-        self.odometry.setPose(pose)
+    #     rospy.loginfo('Setting initial pose to %s', pose)
+    #     self.odometry.setPose(pose)
 
     def rightCallback(self, msg):
-        global left
-        left = left+msg.data 
-        self.odometry.updateLeftWheel(left)
+	global left
+	left = left+msg.data
+	#rospy.loginfo("    left encoderis: %5.7f",left)   
+	self.odometry.updateLeftWheel(left)
 	
 
     def leftCallback(self, msg):
-        global right
+	global right
         right = right+msg.data
-        self.odometry.updateRightWheel(right)
-
-    # def imu(self, msg):
-    #     global sequence, frameid, angx, angy, angz, linx, liny, linz
-    #     #header
-    #     sequence=msg.header.seq
-    #     frameid=msg.header.frame_id
-    #     #data
-    #     angx=msg.angular_velocity.x
-    #     angy=msg.angular_velocity.y
-    #     angz=msg.angular_velocity.z
-    #     linx=msg.linear_acceleration.x
-    #     liny=msg.linear_acceleration.y
-    #     linz=msg.linear_acceleration.z
+	#rospy.loginfo("    right encoderis: %5.7f",right)
+	self.odometry.updateRightWheel(right)
 
 
 
